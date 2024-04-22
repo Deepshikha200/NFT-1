5300a98bf1895712b722665840fdc39a5df93422
Deepshikha200 <157801774+Deepshikha200@users.noreply.github.com>
2024-04-22 14:11:29 +0530

    initial commit

---
+++ context/constants.js
@@ -0,0 +1,6 @@
+import market from './NFTMarketplace.json';
+
+export const MarketAddress = '0x50c029ea507a767cd055b6f6e95b51b4061bd2d4';
+export const MarketAddressABI = market.abi;
+
+// export const MarketAddress = '0x38cf081d8fafc782d4c888ccd7535d0128555850';
